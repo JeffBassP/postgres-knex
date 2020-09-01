1929fea63157f53194c90896ab9675082991521c
Jeferson <jeferson_jfs@hotmail.com>
2020-09-01 20:59:49 -0300

    aplying knex and postgres on API that was use mong

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,8 +1,81 @@
-const getUSer = async () => ({
-    name: 'nome do usuário',
-    idade: 21
-});
+const knex = require('knex');
+const tableName = 'usuarios';
+
+let queryBuilder;
+
+const connectDatabase = async () => {
+    queryBuilder = queryBuilder || knex({
+        client: 'pg',
+        connection: 'postgres://lnqqpxag:eABvYAtS5HNX5X7c4Mcm4K3IOGNJ6XL0@lallah.db.elephantsql.com:5432/lnqqpxag',
+        searchPath: ['knex', 'public'],
+    });
+
+
+    return queryBuilder;
+};
+
+
+const getUSer = async () => {
+    const queryBuilder = await connectDatabase();
+    return queryBuilder.select('*').from(tableName);
+};
+
+const createUSer = async ({
+    name,
+    lastname
+}) => {
+    const queryBuilder = await connectDatabase();
+    return queryBuilder(tableName).insert({
+        name,
+        lastname
+    });;
+};
+
+const updateUser = async ({
+    id
+}, {
+    name,
+    lastname
+}) => {
+    try {
+        const queryBuilder = await connectDatabase();
+        await queryBuilder(tableName)
+            .where({
+                id
+            })
+            .update({
+                name,
+                lastname
+            });
+        return {
+            messa: 'update feito com sucesso'
+        };
+    } catch (error) {
+        throw new Error('Deu ruim');
+    }
+};
+
+const deleteUser = async ({
+    id
+}) => {
+    try {
+        const queryBuilder = await connectDatabase();
+        await queryBuilder(tableName)
+            .where({
+                id
+            })
+            .del();
+        return {
+            messa: 'Usuário removido com sucesso '
+        };
+    } catch (error) {
+        throw new Error('Deu ruim');
+    }
+};
 
 module.exports = {
-    getUSer
+    getUSer,
+    createUSer,
+    updateUser,
+    deleteUser
 }
